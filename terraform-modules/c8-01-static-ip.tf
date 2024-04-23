143e33f1b26c6d8c0f4da47012f0f970890dcbd8
jangmi <paspa96@naver.com>
2024-04-24 01:18:04 +0900

    static IP 버그 수정

--- terraform-modules/c8-01-static-ip.tf
+++ terraform-modules/c8-01-static-ip.tf
@@ -1,6 +1,6 @@
 resource "google_compute_address" "static" {
-  name = "nat-auto-ip"
-  prefix_length = 29
+  name = "gke-ingress-extip1"
+  # prefix_length = 29
   address_type = "EXTERNAL"
-  network = google_compute_network.vpc.self_link
+  # network = google_compute_network.vpc.self_link
 }
