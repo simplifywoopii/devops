151bf6395046f4c41f9e3ef5b63cefd2a06ceb69
jangmi <paspa96@naver.com>
2024-04-24 00:47:27 +0900

    NAT gateway와 router추가

---
+++ terraform-modules/c7-01-cloud-router.tf
@@ -0,0 +1,9 @@
+resource "google_compute_router" "router" {
+  name    = "gke-us-central1-cloud-router"
+  region  = google_compute_subnetwork.subnet.region
+  network = google_compute_network.vpc.id
+
+  bgp {
+    asn = 64514
+  }
+}
---
+++ terraform-modules/c7-02-cloud-nat-gw.tf
@@ -0,0 +1,16 @@
+resource "google_compute_router_nat" "nat" {
+  name                               = "gke-us-central1-default-cloudnat-gw"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+
+  enable_dynamic_port_allocation = true
+  min_ports_per_vm = 64
+  max_ports_per_vm = 65536
+}
