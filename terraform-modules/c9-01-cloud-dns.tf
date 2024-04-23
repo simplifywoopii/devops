2bccef10cda44269a079bc010c67c3da6eea792d
jangmi <paspa96@naver.com>
2024-04-24 01:41:39 +0900

    google DNS 업데이트 A레코드 추가

---
+++ terraform-modules/c9-01-cloud-dns.tf
@@ -0,0 +1,23 @@
+# resource "google_dns_managed_zone" "prod" {
+#   name     = "simplifywoopii"
+#   dns_name = "simplifywoopii.com."
+# }
+
+
+locals {
+  simplifywoopii_dns_managed_zone = {
+    name = "simplifywoopii" 
+    dns_name = "simplifywoopii.com."
+  } 
+}
+
+
+resource "google_dns_record_set" "frontend" {
+  name = "demo1.${local.simplifywoopii_dns_managed_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = local.simplifywoopii_dns_managed_zone.name
+
+  rrdatas = [google_compute_address.static.address]
+}
