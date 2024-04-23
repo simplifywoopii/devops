7d305a9db885167892d5fff79bc425ab1f90ad5e
whoopii <woohyeon88@daolcompany.com>
2024-04-23 21:40:27 +0900

    변수 추가

--- terraform-modules/c3-generic-variables.tf
+++ terraform-modules/c3-generic-variables.tf
@@ -6,6 +6,10 @@ variable "region" {
   description = "region"
 }
 
+variable "azs" {
+  description = "available zones"
+}
+
 variable "gke_num_nodes" {
   default     = 3
   description = "number of gke nodes"
@@ -18,4 +22,11 @@ variable "gke_machine_type" {
 
 variable "owners" {
   description = "owner"
+}
+
+variable "master" {
+  type = map(string)
+  default = {
+    "master_ipv4_cidr_block" = "172.16.0.0/28"
+  }
 }
