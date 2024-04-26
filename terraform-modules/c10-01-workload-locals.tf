bd3135c749c08bfa5a7f5dc78cc1728fc05366d1
whoopii <woohyeon88@daolcompany.com>
2024-04-27 02:00:19 +0900

    불필요한 파일 정리

--- terraform-modules/c10-01-workload-locals.tf
+++
@@ -1,6 +0,0 @@
-# locals {
-#   workload_identity ={
-#     service_account_name = "wid-ksa"
-#     service_account_namespace = "wid-kns"
-#   }
-# }
--- terraform-modules/c13-07-argocd-outputs.tf
+++
@@ -1,5 +0,0 @@
-# # Helm Release Outputs
-# output "argocd_helm_metadata" {
-#   description = "Metadata Block outlining status of the deployed release."
-#   value       = helm_release.argocd.metadata
-# }
