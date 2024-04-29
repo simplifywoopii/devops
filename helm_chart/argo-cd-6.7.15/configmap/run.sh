d98c0549c99d0c147cecb7bfdeb2b8e0f7e21fba
simplifywoopii <simplifywoopii@gmail.com>
2024-04-29 16:44:06 +0900

    helm 배포 shell script

--- helm_chart/argo-cd-6.7.15/configmap/run.sh
+++ helm_chart/argo-cd-6.7.15/configmap/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+CURRENT_WORKDIR=$(pwd) # /Users/whoopi/simplifywoopii
+ARGODIR=${CURRENT_WORKDIR}/helm_chart/argo-cd-6.7.15/configmap
 
-kubectl delete -f argocd-cm.yaml
-kubectl delete -f argocd-rbac-cm.yaml
 
-kubectl create -f argocd-cm.yaml --save-config
-kubectl create -f argocd-rbac-cm.yaml --save-config
+kubectl delete -f ${ARGODIR}/argocd-cm.yaml
+kubectl delete -f ${ARGODIR}/argocd-rbac-cm.yaml
+
+kubectl create -f ${ARGODIR}/argocd-cm.yaml --save-config
+kubectl create -f ${ARGODIR}/argocd-rbac-cm.yaml --save-config
 
 kubectl rollout restart deployment argocd-server -n argocd
 kubectl rollout restart deployment argocd-applicationset-controller -n argocd
