fdc64623fa3f5ec0303c1c5a6eba375bc0d5bc21
whoopii <woohyeon88@daolcompany.com>
2024-05-02 21:05:59 +0900

    쓸모없는 파일 삭제

--- pulumi/history.sh
+++
@@ -1,7 +0,0 @@
-# # project setting
-# pulumi config set gcp:project devops-421112
-
-# # pulumi backend cli docs
-# # https://www.pulumi.com/docs/cli/environment-variables/
-
-# pulumi new kubernetes-gcp-python
