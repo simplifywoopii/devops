0faa6f68c14b677721e1aeb13a90bcef5c1038c6
simplifywoopii <simplifywoopii@gmail.com>
2024-05-02 17:43:18 +0900

    shell script update

--- run.sh
+++ run.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
+if [[ $1 == "all" ]]; then
+    docker compose build --no-cache
+    docker compose run --rm gke
+    docker compose run --rm gke sh -c "terraform apply -auto-approve"
+fi
+
+
+
 CURRENT_WORKDIR=$(pwd)
-HELM_REPO="${CURRENT_WORKDIR}/helm_chart"
-ARGOCD=${HELM_REPO}/argo-cd-6.7.15
-CONFIGMAP=${ARGOCD}/configmap 
-EXTENRAL_DNS=${HELM_REPO}/external-dns-7.2.0
+ARGOCD_CONFIGMAP_DIR="${CURRENT_WORKDIR}/argocd_configmap"
+
+# gke authenticate
+sleep 1s
+gcloud container clusters get-credentials ${GOOGLE_GKE_CLUSTER_NAME} --region ${GOOGLE_REGION} --project ${GOOGLE_PROJECT_ID}
+sleep 3s
 
 
-terraform run
-cd terraform-modules
-terraform init -backend-config backend.conf
-terraform apply -auto-approve
-cd ..
-
-
-sleep 5s
-gcloud container clusters get-credentials simplifywoopii-devops-gke --region us-central1 --project devops-421112
-
-sleep 5s
 # helm 여부 확인
-exists=$(helm ls -n external-dns-ns | awk 'BEGIN {count=0} { if ($1 == "external-dns") count+=1} END  {print count}')
-if [[ $exists == '1' ]];then
-    helm upgrade external-dns -f "${EXTENRAL_DNS}/ci/override_values.yaml" -n external-dns-ns ${EXTENRAL_DNS}
-else
-    helm install external-dns -f "${EXTENRAL_DNS}/ci/override_values.yaml" -n external-dns-ns --create-namespace ${EXTENRAL_DNS}
+if [[ $1 == "argo" ]] || [[ $1 == "all" ]]; then
+    exists=$(helm ls -n ${GOOGLE_HELM_ARGOCD_NAMESPACE} | awk '{if ($1 == "argocd")  print $1}')
+    if [[ $exists == ${GOOGLE_HELM_ARGOCD_NAME} ]];then
+        # helm chart가 존재
+        kubectl delete -f ${ARGOCD_CONFIGMAP_DIR}/argocd-cm.yaml
+        kubectl delete -f ${ARGOCD_CONFIGMAP_DIR}/argocd-rbac-cm.yaml
+
+        kubectl create -f ${ARGOCD_CONFIGMAP_DIR}/argocd-cm.yaml --save-config
+        kubectl create -f ${ARGOCD_CONFIGMAP_DIR}/argocd-rbac-cm.yaml --save-config
+
+        echo "\n\n[INFO] Argocd rollout restart\n\n"
+
+        kubectl rollout restart deployment argocd-server -n ${GOOGLE_HELM_ARGOCD_NAMESPACE}
+        kubectl rollout restart deployment argocd-applicationset-controller -n ${GOOGLE_HELM_ARGOCD_NAMESPACE}
+        kubectl rollout restart deployment argocd-dex-server -n ${GOOGLE_HELM_ARGOCD_NAMESPACE}
+        kubectl rollout restart deployment argocd-notifications-controller -n ${GOOGLE_HELM_ARGOCD_NAMESPACE}
+        kubectl rollout restart deployment argocd-redis -n ${GOOGLE_HELM_ARGOCD_NAMESPACE}
+        kubectl rollout restart deployment argocd-repo-server -n ${GOOGLE_HELM_ARGOCD_NAMESPACE}
+    fi
 fi
-
-
-
-sleep 5s
-exists=$(helm ls -n argocd | awk 'BEGIN {count=0} { if ($1 == "argocd") count+=1} END  {print count}')
-if [[ $exists == '1' ]];then
-    helm upgrade argocd -f "${ARGOCD}/ci/override_values.yaml" -n argocd ${ARGOCD}
-else
-    helm install argocd -f "${ARGOCD}/ci/override_values.yaml" -n argocd --create-namespace ${ARGOCD}
-fi
-
-sleep 10s
-bash ${CONFIGMAP}/run.sh
-
