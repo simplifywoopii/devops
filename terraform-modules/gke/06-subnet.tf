91fb11ab7f1b689de499ce5a71009742654520a4
whoopii <woohyeon88@daolcompany.com>
2024-05-06 00:41:32 +0900

    gke refactoring

--- terraform-modules/gke/06-subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${local.name}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.128.0.0/20"
-}
--- terraform-modules/gke/20-artifact-registry.tf
+++
@@ -1,7 +0,0 @@
-resource "google_artifact_registry_repository" "simplifywoopii_repo" {
-  depends_on    = [helm_release.argocd]
-  location      = var.region
-  repository_id = "simplifywoopii-apps"
-  description   = "simplifywoopii-apps"
-  format        = "DOCKER"
-}
